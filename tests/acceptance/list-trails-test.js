4ff6c6a5cffc825e12e0a80e3730324d069baa0d
Ben Adamski <benjadamski@gmail.com>
2017-07-08 19:41:56 -0400

    Add small annotation on moduleForAcceptance()
    - Explains what the function does. A previous example showed a test generated
    with `foo-bars` passing in just that. This was generated with `list-trails`
    yet it generated hyphen-less in this function 🤔

--- tests/acceptance/list-trails-test.js
+++ tests/acceptance/list-trails-test.js
@@ -1,6 +1,9 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'trailtracker-client/tests/helpers/module-for-acceptance';
 
+// this function ensure ember app is started and shut
+// down properly between each test.
+// should it pass in list-trails? hyphen?
 moduleForAcceptance('Acceptance | list trails');
 
 test('visiting /list-trails', function(assert) {
